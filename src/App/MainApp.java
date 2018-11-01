f00390c7c11de3ca1e5d32692cb4825e568ef379
Olexandr Burlaka <wrkttt@gmail.com>
2018-11-01 23:44:30 +0200

    First commit

--- src/App/MainApp.java
+++ src/App/MainApp.java
@@ -5,7 +5,7 @@ import App.core.Window;
 
 public class MainApp {
     public static void main(String[] args) {
-        Window window1 = new Window("Test", 640, 480);
+        Window window1 = new Window("New Window", 480, 360);
         window1.addLayer(new Example());
     }
 }
